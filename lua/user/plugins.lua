eb4a31769e01300083a407c1186dce0b4084ba92
Lawrence Lim <ll4715@nyu.edu>
2022-07-13 01:44:55 +0800

    Reorganized plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -37,6 +37,7 @@ packer.init {
 }
 
 return packer.startup(function(use)
+
   -- Core Plugins
   use { "wbthomason/packer.nvim" }
   use { "nvim-lua/popup.nvim" }
@@ -48,42 +49,61 @@ return packer.startup(function(use)
   use { "folke/tokyonight.nvim" }
   use { "catppuccin/nvim", as = "catppuccin" }
   use { "ful1e5/onedark.nvim" }
+
+  -- IDE Features
   use { "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } }
   use { "akinsho/toggleterm.nvim", tag = "v1.*" }
+  use { "numToStr/Comment.nvim" }
+  use { "preservim/tagbar" }
+  use { "kyazdani42/nvim-tree.lua", requires = { "kyazdani42/nvim-web-devicons" } }
+  use { "pseewald/vim-anyfold" }
+  use { "fedepujol/move.nvim" }
+  use { "windwp/nvim-autopairs" }
+
+  -- LSP
+  use { "neovim/nvim-lspconfig" }
+  use { "hrsh7th/cmp-nvim-lsp" }
+  use { "williamboman/nvim-lsp-installer" }
+  -- use { "github/copilot.vim" }
+  -- use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
+
+  -- CMP
   use { "hrsh7th/nvim-cmp" }
   use { "hrsh7th/cmp-buffer" }
   use { "hrsh7th/cmp-path" }
   use { "hrsh7th/cmp-cmdline" }
   use { "hrsh7th/cmp-nvim-lua" }
-  use { "numToStr/Comment.nvim" }
-  use { "preservim/tagbar" }
+
+  -- Snippets
+  use { "saadparwaiz1/cmp_luasnip" }
+  use { "rafamadriz/friendly-snippets" }
+  use { "L3MON4D3/LuaSnip" }
+
+  -- Git
+  use { "lewis6991/gitsigns.nvim" }
+  use { "tpope/vim-fugitive" }
+
+  -- Telescope
+  use { "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } }
+  use { "nvim-telescope/telescope-ui-select.nvim" }
+  use { "ThePrimeagen/harpoon" }
+  use { "ThePrimeagen/refactoring.nvim",
+    requires = { { "nvim-lua/plenary.nvim" }, { "nvim-treesitter/nvim-treesitter" } } }
+
+  -- Additional Features
   use { "Pocco81/AutoSave.nvim" }
-  use { "fedepujol/move.nvim" }
+  use { "junegunn/goyo.vim" }
+  use { "tpope/vim-obsession" }
+  use { "vimwiki/vimwiki" }
+  use { "psf/black", branch = "stable" }
+  use { "lukas-reineke/indent-blankline.nvim" }
+  use { "norcalli/nvim-colorizer.lua" }
+
+  -- Debugger
   use { "mfussenegger/nvim-dap" }
   use { "mfussenegger/nvim-dap-python" }
   use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
-  use { "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } }
-  -- use { "pseewald/vim-anyfold" }
-  use { "nvim-telescope/telescope-ui-select.nvim" }
-  use { "ThePrimeagen/harpoon" }
-  use { "ThePrimeagen/refactoring.nvim", requires = { { "nvim-lua/plenary.nvim" }, { "nvim-treesitter/nvim-treesitter" } } }
-  use { "kyazdani42/nvim-tree.lua", requires = { "kyazdani42/nvim-web-devicons" } }
-  use { "saadparwaiz1/cmp_luasnip" }
-  use { "L3MON4D3/LuaSnip" }
-  use { "rafamadriz/friendly-snippets" }
-  use { "neovim/nvim-lspconfig" }
-  use { "hrsh7th/cmp-nvim-lsp" }
-  use { "williamboman/nvim-lsp-installer" }
-  use { "windwp/nvim-autopairs" }
-  use { "junegunn/goyo.vim" }
-  use { "lewis6991/gitsigns.nvim" }
-  use { "tpope/vim-fugitive" }
-  use { "tpope/vim-obsession" }
-  use { "vimwiki/vimwiki"}
-  use { "github/copilot.vim"}
-  use { "psf/black", branch = "stable" }
 
-  -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
