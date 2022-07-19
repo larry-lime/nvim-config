3ce518e5a2b25a75c4545c503296fdf780b1ce7e
Lawrence Lim <ll4715@nyu.edu>
2022-07-19 19:41:07 +0800

    Updated included filetypes

--- lua/user/lsp/settings/tailwindcss.lua
+++ lua/user/lsp/settings/tailwindcss.lua
@@ -1,2 +1,8 @@
-return {}
+return {
+  fileyptes = { "aspnetcorerazor", "astro", "astro-markdown", "blade", "django-html", "htmldjango", "edge", "eelixir",
+    "ejs", "erb", "eruby", "gohtml", "haml", "handlebars", "hbs", "html-eex", "heex", "jade", "leaf", "liquid",
+    "markdown", "mdx", "mustache", "njk", "nunjucks", "php", "razor", "slim", "twig", "css", "less", "postcss", "sass",
+    "scss", "stylus", "sugarss", "reason", "rescript", "vue", "svelte" }
+  -- javascript, typescript, typescriptreact, javascriptreact, html
+}
 -- require'lspconfig'.tailwindcss.setup{}
