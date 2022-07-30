4fe82992f03e1ab87eaba1e1c1627024bbcd7be0
Lawrence Lim <ll4715@nyu.edu>
2022-07-30 12:43:43 +0800

    Reinstalled prettier plugin

---
+++ lua/user/plug/prettier.lua
@@ -0,0 +1,23 @@
+local null_ls = require("null-ls")
+local prettier = require("prettier")
+
+null_ls.setup({})
+
+prettier.setup({
+  bin = 'prettier', -- or `prettierd`
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+    "solidity",
+  },
+})
