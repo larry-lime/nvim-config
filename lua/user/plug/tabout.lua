89174812aae5b9d2d32655ce63e0fbc63e7a8576
Lawrence Lim <ll4715@nyu.edu>
2022-07-30 12:43:25 +0800

    Updated to work well with snippet jumps

--- lua/user/plug/tabout.lua
+++ lua/user/plug/tabout.lua
@@ -1,10 +1,10 @@
  require('tabout').setup {
     tabkey = '<C-t>', -- key to trigger tabout, set to an empty string to disable
     backwards_tabkey = '<C-d>', -- key to trigger backwards tabout, set to an empty string to disable
-    act_as_tab = true, -- shift content if tab out is not possible
-    act_as_shift_tab = true, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-    default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-    default_shift_tab = '<C-d>', -- reverse shift default action,
+    act_as_tab = false, -- shift content if tab out is not possible
+    act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+    -- default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+    -- default_shift_tab = '<C-d>', -- reverse shift default action,
     enable_backwards = true, -- well ...
     completion = true, -- if the tabkey is used in a completion pum
     tabouts = {
