95f13b00915d11de9cda27c531646cf0b2834a56
Lawrence Lim <you@example.com>
2023-01-12 01:52:05 +0800

    Updated lualine

--- lua/user/plug/lualine.lua
+++ lua/user/plug/lualine.lua
@@ -13,12 +13,12 @@ require('lualine').setup {
     globalstatus = true,
   },
   sections = {
-    lualine_a = { { 'mode', color = { fg = 'NonText' } } },
+    lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
     lualine_c = { 'filename' },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
-    lualine_z = { { 'location', color = {  fg = 'NonText'  } } },
+    lualine_z = { 'location' },
   },
   tabline = {
     -- lualine_a = {
