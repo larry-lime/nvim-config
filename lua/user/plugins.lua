db9c78f97405f3df82f414a32a5d768d282c4dc9
Lawrence Lim <ll4715@nyu.edu>
2023-01-28 22:52:42 +0800

    Updated move nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -58,7 +58,7 @@ return packer.startup(function(use)
   use { "kyazdani42/nvim-tree.lua", requires = { "kyazdani42/nvim-web-devicons" } }
   use { "windwp/nvim-autopairs" }
   use { "akinsho/toggleterm.nvim", tag = "v1.*" }
-  use { "hinell/move.nvim" }
+  use { 'fedepujol/move.nvim' }
   use { 'rmagatti/auto-session' }
 
   -- LSP
