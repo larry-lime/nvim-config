b62d14c92bfa57e1f22eae00f0198969b4768e7c
Your Name <your@example.coMm>
2023-04-08 06:26:48 +0000

    Updated vim-fugitive keybinds

--- lua/user/plug/telescope.lua
+++ lua/user/plug/telescope.lua
@@ -93,7 +93,7 @@ keymap("n", "<leader>jl", ":Telescope jumplist<CR>", opts)
 keymap("n", "<leader>mp", ":Telescope keymaps<CR>", opts)
 keymap("n", "<leader>sc", ":Telescope spell_suggest<CR>", opts)
 keymap("n", "<leader>hl", ":Telescope help_tags<CR>", opts)
-keymap("n", "<leader>gs", ":Telescope git_status<CR>", opts)
+-- keymap("n", "<leader>gs", ":Telescope git_status<CR>", opts)
 
 -- Notes
 keymap("n", "<leader>td", ":lua require('telescope.builtin').grep_string({prompt_title='TODOs',search='TODO'})<CR>", opts)
